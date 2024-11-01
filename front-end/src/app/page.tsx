fa4122d1cb8a9edf594480ea5de846091b1b2d97
Wallace143900 <hiperguardi@gmail.com>
2024-11-01 18:39:01 -0300

    adicionei um loading

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import AllThePower from "./components/AllThePower";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -5,18 +7,35 @@ import MakeSomething from "./components/MakeSomething";
 import PickTheSun from "./components/PickTheSun";
 import Powerful from "./components/Powerful";
 import PServices from "./components/PServices";
+import LoadingComponent from "./components/LoadingComponent";
 
 
 export default function Home() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); 
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Header />
-      <PickTheSun />
-      <PServices />
-      <Powerful />
-      <MakeSomething />
-      <AllThePower />
-      <Footer />
+      {loading ? (
+        <LoadingComponent />
+      ) : (
+        <>
+          <Header />
+          <PickTheSun />
+          <PServices />
+          <Powerful />
+          <MakeSomething />
+          <AllThePower />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
